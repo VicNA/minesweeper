733f19ee8f4057431e57e661c6bedb54d8837f20
viktor-n <vnikolaeff989@gmail.com>
2023-02-08 11:24:01 +0300

    Project: Minesweeper. Topic: Random. Complete

---
+++ Topics/Random/The Rock-Paper-Scissors game/src/Main.kt
@@ -0,0 +1,9 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+fun makeDecision(): String =
+    when (Random.nextInt(1..3)) {
+        1 -> "Rock"
+        2 -> "Paper"
+        else -> "Scissors"
+    }
