89ad47ea95c0f67d0da084feeee05ccc94e25be9
viktor-n <vnikolaeff989@gmail.com>
2023-02-09 11:20:09 +0300

    Project: Minesweeper. Stage 2/5. Complete

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,13 +1,26 @@
 package minesweeper
 
+import kotlin.random.Random
+
 fun main() {
-    println(".X.......\n" +
-            ".....X..X\n" +
-            "....X....\n" +
-            "......X..\n" +
-            "..X......\n" +
-            "....X....\n" +
-            "..X......\n" +
-            "..X......\n" +
-            "......X..")
+    val field = Array(9) {
+        Array(9) { '.' }
+    }
+
+    print("How many mines do you want on the field? ")
+    var mine = readln().toInt()
+    var x = 0
+    var y = 0
+
+    while (mine > 0) {
+        x = Random.nextInt(field.size)
+        y = Random.nextInt(field.size)
+
+        if (field[x][y] != 'X') {
+            field[x][y] = 'X'
+            mine--
+        }
+    }
+
+    field.forEach { println(it.joinToString("")) }
 }
