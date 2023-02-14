cb349f3ebc48bfdd733d9f2666d99c5e46401568
viktor-n <vnikolaeff989@gmail.com>
2023-02-14 17:16:52 +0300

    Project: Minesweeper. Stage 3/5. Progress v.1

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,24 +1,18 @@
 package minesweeper
 
-import kotlin.random.Random
-
 fun main() {
-    val field = Array(9) {
-        Array(9) { '.' }
-    }
-
     print("How many mines do you want on the field? ")
     var mine = readln().toInt()
-    var x = 0
-    var y = 0
+    val field = ("X".repeat(mine) + "0".repeat(9 * 9 - mine)).toList().shuffled()
+        .chunked(9).map { it.toTypedArray() }
 
-    while (mine > 0) {
-        x = Random.nextInt(field.size)
-        y = Random.nextInt(field.size)
-
-        if (field[x][y] != 'X') {
-            field[x][y] = 'X'
-            mine--
+    for (i in 0..field.lastIndex) {
+        for (j in 0..field[i].lastIndex) {
+            if (field[i][j] == 'X') {
+                if (i + 1 < field.lastIndex &&  j + 1 < field[i].lastIndex && field[i+ 1][j + 1] != 'X') field[i + 1][j + 1]++
+                if (j + 1 < field[i].lastIndex && field[i][j + 1] != 'X') field[i][j + 1]++
+                if (i + 1 < field.lastIndex && field[i + 1][j] != 'X') field[i + 1][j]++
+            }
         }
     }
 
