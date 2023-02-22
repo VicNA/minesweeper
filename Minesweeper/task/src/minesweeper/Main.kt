8e818653acd0dae0e7b25ef3690b5d5ef6b6b03d
viktor-n <vnikolaeff989@gmail.com>
2023-02-22 09:13:24 +0300

    Project: Minesweeper. Stage 3/5. Complete

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -10,8 +10,6 @@ fun main() {
     print("How many mines do you want on the field? ")
     var mine = readln().toInt()
     var (row, col, x, y) = arrayOf(0, 0, 0, 0)
-//    var x = 0
-//    var y = 0
 
     while (mine > 0) {
         row = Random.nextInt(field.size)
@@ -22,9 +20,12 @@ fun main() {
             mine--
 
             for (i in -1..1) {
-                x = (row + i).let { if (it < 0 || it > field.lastIndex) row else it } // значения выпадающие за границу, пропускать
+                x = row + i
+                if (x !in field.indices) continue
+
                 for (j in -1..1) {
-                    y = (col + j).let { if (it < 0 || it > field.lastIndex) col else it } // значения выпадающие за границу, пропускать
+                    y = col + j
+                    if (y !in field.indices) continue
 
                     if (field[x][y] != 'X' && field[x][y] != '.') {
                         field[x][y]++
@@ -36,5 +37,5 @@ fun main() {
         }
     }
 
-    field.forEach { println(it.joinToString(" ")) }
+    field.forEach { println(it.joinToString("")) }
 }
