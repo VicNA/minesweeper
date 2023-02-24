e92293a2c9ef1a22332d6a8c3bb810f39833d784
VicN <vnikolaeff989@gmail.com>
2023-02-24 20:54:22 +0300

    Project: Minesweeper. Stage 4/5. Progress v.1

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -3,39 +3,52 @@ package minesweeper
 import kotlin.random.Random
 
 fun main() {
-    val field = Array(9) {
-        Array(9) { '.' }
-    }
+    val field = Array(9) { Array(9) { '.' } }
+    val mines = Array(9) { Array(9) { '.' } }
 
     print("How many mines do you want on the field? ")
     var mine = readln().toInt()
     var (row, col, x, y) = arrayOf(0, 0, 0, 0)
 
-    while (mine > 0) {
-        row = Random.nextInt(field.size)
-        col = Random.nextInt(field.size)
+//    while (mine > 0) {
+//        row = Random.nextInt(field.size)
+//        col = Random.nextInt(field.size)
+//
+//        if (field[row][col] != 'X') {
+//            field[row][col] = 'X'
+//            mine--
+//
+//            for (i in -1..1) {
+//                x = row + i
+//                if (x !in field.indices) continue
+//
+//                for (j in -1..1) {
+//                    y = col + j
+//                    if (y !in field.indices) continue
+//
+//                    if (field[x][y] != 'X' && field[x][y] != '.') {
+//                        field[x][y]++
+//                    } else if (field[x][y] == '.') {
+//                        field[x][y] = '1'
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    field.forEach { println(it.joinToString("")) }
+}
 
-        if (field[row][col] != 'X') {
-            field[row][col] = 'X'
-            mine--
+fun printField(field: Array<Array<Char>>) {
+    val hline = "—"
+    val vline = "│"
 
-            for (i in -1..1) {
-                x = row + i
-                if (x !in field.indices) continue
+    println(" $vline${(1..field.size).joinToString("")}$vline")
+    println("$hline$vline${hline.repeat(field.size)}$vline")
 
-                for (j in -1..1) {
-                    y = col + j
-                    if (y !in field.indices) continue
-
-                    if (field[x][y] != 'X' && field[x][y] != '.') {
-                        field[x][y]++
-                    } else if (field[x][y] == '.') {
-                        field[x][y] = '1'
-                    }
-                }
-            }
-        }
+    for (i in field.indices) {
+        println("${i + 1}$vline${field[i].joinToString("")}$vline")
     }
 
-    field.forEach { println(it.joinToString("")) }
+    println("$hline$vline${hline.repeat(field.size)}$vline")
 }
